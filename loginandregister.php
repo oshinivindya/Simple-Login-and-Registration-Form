e6552943977c177018f6f5217e8ab6ae838ab8cc
oshini premathilake <109908094+oshinivindya@users.noreply.github.com>
2023-02-09 13:01:35 +0530

    Add files via upload

---
+++ loginandregister.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Connecting Login and Registration Form to Database-----SeekCoding</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="full-page">
+        <div class='navbar'>
+            <div class='logo'>
+                <a href='#'><h1>Login always</h1></a>
+            </div>
+            <nav>
+                <ul id='MenuItems'>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About us</a></li>
+                    <li><a href='#'>Products</a></li>
+                    <li><a href='#'>Offers</a></li>
+                    <li><button class='loginbtn'onclick="document.getElementById('login-form').style.display='block'" style="width:auto;">Login</button></li>
+                    <li><button class='loginbtn'onclick="document.getElementById('register-form').style.display='block'" style="width:auto;">Register</button></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="sub-page">
+          <div class="overlay"></div>
+          <br><br><br>
+           <div class="text">
+
+               <p>Music is poetry,<br>
+                An expression of the heart.<br>
+                I can feel it in me when the music starts.<br>
+                My blood is flowing,<br>
+                My mind is free.<br>
+                It's beautiful when someone<br>
+                Is feeling the same as me.<br>
+                Words are the pristine way to uplift<br>
+                An emotion.<br>
+                My words are my heart.<br>
+                My paper is my sleeve.<br>
+                I wonder if someone feels the same as me</p>
+            </div>
+        </div>
+        <div id='login-form' class="login-page">
+            <div class="form-box">
+                <span onclick="document.getElementById('login-form').style.display='none'" class="close">&times;</span>
+                <div class="form">
+                    <form class='login-form', action="validate.php", method="post">
+                        <center><h1 class="main-heading">Login Form</h1></center>
+				        <input type="text"name='emailid' placeholder="Email ID", required/>
+				        <input type="password" name='password' placeholder="password", required/>
+				        <button>LOGIN</button>
+				    </form>
+                </div>
+            </div>
+        </div>
+        <div id="register-form" class='register-page'>
+            <div class="form-box1">
+               <span onclick="document.getElementById('register-form').style.display='none'" class="close">&times;</span>
+                <div class="form1">
+                    <form class='register-form', action="registration.php", method="post">
+                        <center><h1 class="main-heading">Register Form</h1></center>
+				        <input type="text" name='user'placeholder="user name" required/>
+				        <input type="text" name='emailid'placeholder="email-id" required/>
+				        <input type="password"name='password' placeholder="password" required/>
+				        <button>REGISTER</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
